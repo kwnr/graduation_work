d4acf9e025c47e2ecb667f777a3db003e4714cf2
Hyeokbeom <khb100100@naver.com>
2023-03-22 12:39:34 +0900

    initial commit

---
+++ image_detecting/assuming_target_rotate.py
@@ -0,0 +1,82 @@
+import cv2
+import numpy as np
+import time
+from scipy.spatial.transform import Rotation as R
+
+def setLabel(img, pts, label):
+    # 사각형 좌표 받아오기
+    (x, y, w, h) = cv2.boundingRect(pts)
+    pt1 = (x, y)
+    pt2 = (x + w, y + h)
+    cv2.rectangle(img, pt1, pt2, (0, 0, 255), 1)
+    cv2.putText(img, label, pt1, cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
+    
+
+cap=cv2.VideoCapture(1)
+frame_rate=10
+prev=0
+    
+while cap.isOpened():
+    time_elapsed = time.time() - prev   #10FPS로 제한
+    res, image = cap.read()             #
+
+    if time_elapsed > 1./frame_rate:
+        prev = time.time()
+
+    status, img=cap.read()
+    
+    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(img_gray, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+    contours,hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    for i in range(len(contours)):
+        if cv2.contourArea(contours[i]) < 400: # 노이즈 제거, 너무 작으면 무시
+            continue
+        eps=cv2.arcLength(contours[i],True)
+        approx=cv2.approxPolyDP(contours[i],epsilon=eps*0.03,closed=True)
+        vtc=len(approx)
+        if vtc==12:
+            M=cv2.moments(contours[i])  #꼭짓점이 12개인 폐곡선 컨투어의 이미지 모멘트 계산
+            cx=int(M['m10']/M['m00'])   #폐곡선의 중심 계산
+            cy=int(M['m01']/M['m00'])
+            cv2.circle(img,(cx,cy),1,(0,0,255),2) 
+            cv2.drawContours(img, [contours[i]], 0, (0,0,255), 2)
+            cv2.putText(img, f'AREA: {cv2.contourArea(contours[i])}, COORD: ({cx}, {cy})', tuple(contours[i][0][0]),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,0,0),2)
+            rect=cv2.minAreaRect(contours[i])   #십자를 감싸는 최소 넓이 사각형, https://docs.opencv.org/4.3.0/dd/d49/tutorial_py_contour_features.html 참조
+            box=cv2.boxPoints(rect)
+            box=np.intp(box)
+            cv2.drawContours(img,[box],0,(255,0,0),2)
+            
+
+            sm=box.sum(axis=1)
+            diff=np.diff(box,axis=1)
+
+            topLeft=box[np.argmin(sm)]
+            BottomRight=box[np.argmax(sm)]
+            topRight=box[np.argmin(diff)]
+            BottomLeft=box[np.argmax(diff)]
+
+            pts1=np.float32([topLeft,topRight,BottomRight,BottomLeft])
+
+            w1=abs(BottomLeft[0]-BottomRight[0])
+            w2=abs(topLeft[0]-topRight[0])
+            h1=abs(BottomLeft[1]-topLeft[1])
+            h2=abs(BottomRight[1]-topRight[1])
+            width=max(w1,w2)
+            height=max(h1,h2)
+
+            pts2=np.float32([[0,0],[width-1,0],[width-1,height-1],[0,height-1]])
+
+            rotate_matrix=cv2.getPerspectiveTransform(pts1,pts2)
+            r=R.from_matrix(rotate_matrix)
+            euler_angle=r.as_euler('xyz',True)
+            cv2.putText(img, f'ROLL: {euler_angle[2]:.1f}',
+                        (int(pts1[2][0]),int(pts1[2][1])),cv2.FONT_HERSHEY_PLAIN,2,(0,255,0),2)
+                        
+            
+    cv2.imshow('img',img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break    
+
+cap.release()
+cv2.destroyAllWindows()
