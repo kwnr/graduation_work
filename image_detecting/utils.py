e9c1b011943c9ccba6db2c9b80a605db67e85b9f
Hyeokbeom <khb100100@naver.com>
2023-03-25 01:04:51 +0900

    0325

--- image_detecting/utils.py
+++ image_detecting/utils.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def set_cap_resolution(cam, h)->None:
     '''change capture resolution with 16:9 ratio'''
@@ -7,10 +8,16 @@ def set_cap_resolution(cam, h)->None:
     cam.set(cv2.CAP_PROP_FRAME_HEIGHT,h)
     
     
-
-INDEX_PARAM=dict(
-        algorithm=6,
-        table_number=6,
-        key_size=12,
-        multi_probe_level=1
-    )
+def get_heading(head,center):
+    hx,hy=head
+    cx,cy=center
+    if hy-cy==0:
+        if hx>cx:
+            heading=np.pi
+        else:
+            heading=-np.pi
+    else:
+        heading=np.arctan(np.divide(hx-cx,hy-cy))
+        if hy<cy:
+            heading=-heading
+    return heading
