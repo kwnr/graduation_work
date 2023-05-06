695da7fe73e0ae588fce812a12b9bf4e5447c4eb
Hyeokbeom <khb100100@naver.com>
2023-05-06 20:27:34 +0900

    a

--- sensor_fusion/fusion.py
+++ sensor_fusion/fusion.py
@@ -1,79 +1,55 @@
-import smbus					#import SMBus module of I2C
-from time import sleep          #import
-import kalman_filter
+from read_sensor import MPU6050
+from kalman_filter import KalmanFilter
+from scipy.spatial.transform import Rotation as R
+import numpy as np
+from time import sleep
+import matplotlib.pyplot as plt
 
-#some MPU6050 Registers and their Address
-PWR_MGMT_1   = 0x6B
-SMPLRT_DIV   = 0x19
-CONFIG       = 0x1A
-GYRO_CONFIG  = 0x1B
-INT_ENABLE   = 0x38
-ACCEL_XOUT_H = 0x3B
-ACCEL_YOUT_H = 0x3D
-ACCEL_ZOUT_H = 0x3F
-GYRO_XOUT_H  = 0x43
-GYRO_YOUT_H  = 0x45
-GYRO_ZOUT_H  = 0x47
+sensor=MPU6050()
+kf=KalmanFilter(4,4)
+kf.H=np.eye(4)
+kf.Q=np.eye(4)*0.0001
+kf.R=np.eye(4)*10
+kf.x=np.array([0,0,0,1]).T
+kf.P=np.eye(4)
 
+phi,theta,psi=0,0,0
+dt=0.1
+g=9.81
+phis=[phi]
+thetas=[theta]
+psis=[psi]
 
-def MPU_Init():
-	#write to sample rate register
-	bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
-	
-	#Write to power management register
-	bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
-	
-	#Write to Configuration register
-	bus.write_byte_data(Device_Address, CONFIG, 0)
-	
-	#Write to Gyro configuration register
-	bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
-	
-	#Write to interrupt enable register
-	bus.write_byte_data(Device_Address, INT_ENABLE, 1)
-
-def read_raw_data(addr):
-	#Accelero and Gyro value are 16-bit
-        high = bus.read_byte_data(Device_Address, addr)
-        low = bus.read_byte_data(Device_Address, addr+1)
+while len(phis)<600:
+    Ax,Ay,Az,Gx,Gy,Gz=sensor.read_value()
+    kf.A=np.eye(4)+dt*1/2*np.array([
+        [Gx,0,Gz,-Gy],
+        [Gy,-Gz,0,Gx],
+        [Gz,Gy,-Gz,0],
+        [0,-Gx,-Gy,-Gz]
+    ])
+    theta=np.arcsin(Ax/g)
+    phi=np.arcsin(-Ay/(g*np.cos(theta)))
+    z=R.from_euler('zyx',[theta,phi,0])
+    x=kf.run(z)
+    phi,theta,psi=R.from_quat(x).as_euler('zyx',degrees=True)
+    phis.append(phi)
+    thetas.append(theta)
+    psis.append(psi)
+    sleep(dt)
     
-        #concatenate higher and lower value
-        value = ((high << 8) | low)
-        
-        #to get signed value from mpu6050
-        if(value > 32768):
-                value = value - 65536
-        return value
+euler=np.concatenate((phis,thetas,psis))
+with open("euler.txt",'w') as f:
+    f.write(euler)
 
-
-bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
-Device_Address = 0x68   # MPU6050 device address
-
-MPU_Init()
-
-print (" Reading Data of Gyroscope and Accelerometer")
-
-while True:
-	
-	#Read Accelerometer raw value
-	acc_x = read_raw_data(ACCEL_XOUT_H)
-	acc_y = read_raw_data(ACCEL_YOUT_H)
-	acc_z = read_raw_data(ACCEL_ZOUT_H)
-	
-	#Read Gyroscope raw value
-	gyro_x = read_raw_data(GYRO_XOUT_H)
-	gyro_y = read_raw_data(GYRO_YOUT_H)
-	gyro_z = read_raw_data(GYRO_ZOUT_H)
-	
-	#Full scale range +/- 250 degree/C as per sensitivity scale factor
-	Ax = acc_x/16384.0
-	Ay = acc_y/16384.0
-	Az = acc_z/16384.0
-	
-	Gx = gyro_x/131.0
-	Gy = gyro_y/131.0
-	Gz = gyro_z/131.0
-	
-
-	print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
-	sleep(1)
+plt.figure(1)
+plt.subplot(3,1,1)
+plt.title('phi')
+plt.plot(euler[0,:])
+plt.subplot(3,1,2)
+plt.title('theta')
+plt.plot(euler[1,:])
+plt.subplot(3,1,3)
+plt.title('psi')
+plt.plot(euler[2,:])
+plt.show()
--- sensor_fusion/sensor_test.py
+++ sensor_fusion/sensor_test.py
@@ -5,18 +5,18 @@ from time import sleep
 
 sensor=MPU6050()
 phi,theta,psi=0,0,0
-phis=[0]
-thetas=[0]
-psis=[0]
-dt=0.1;
-while len(psis)<60/dt:
+phis_gryo=[0]
+thetas_gyro=[0]
+psis_gyro=[0]
+dt=0.1
+while len(psis_gyro)<60/dt:
         Ax,Ay,Az,Gx,Gy,Gz=sensor.read_value()
         phi,theta,psi=np.array([[1,np.sin(phi)*np.tan(theta),np.cos(phi)*np.tan(theta)],
                         [0,np.cos(phi),-np.sin(phi)],
                         [0,np.sin(phi)/np.cos(theta),np.cos(phi)/np.cos(theta)]])@np.array([Gx,Gy,Gz]).T
-        phis.append(phis[-1]+phi*dt)
-        thetas.append(thetas[-1]+theta*dt)
-        psis.append(psis[-1]+psi*dt)
+        phis_gryo.append(phis_gryo[-1]+phi*dt)
+        thetas_gyro.append(thetas_gyro[-1]+theta*dt)
+        psis_gyro.append(psis_gyro[-1]+psi*dt)
         print(f'phi: {phi}, theta: {theta}, psi:{psi}')
         sleep(dt)
         
@@ -24,11 +24,11 @@ while len(psis)<60/dt:
 plt.figure(1)
 plt.subplot(3,1,1)
 plt.title('phi')
-plt.plot(phis)
+plt.plot(phis_gryo)
 plt.subplot(3,1,2)
 plt.title('theta')
-plt.plot(thetas)
+plt.plot(thetas_gyro)
 plt.subplot(3,1,3)
 plt.title('psi')
-plt.plot(psis)
+plt.plot(psis_gyro)
 plt.show()
