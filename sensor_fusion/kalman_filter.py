1eacd5c26ed173def73b18a21a569de67e99fbda
Hyeokbeom <khb100100@naver.com>
2023-05-06 20:34:45 +0900

    a

--- sensor_fusion/kalman_filter.py
+++ sensor_fusion/kalman_filter.py
@@ -12,7 +12,7 @@ class KalmanFilter():
     def run(self,z):
         x_pred=self.A@self.x
         P_pred=self.A@self.P@self.A.T+self.Q
-        K=P_pred@self.H.T@np.linalg(self.H@P_pred@self.H.T+self.R)
+        K=P_pred@self.H.T@np.linalg.inv(self.H@P_pred@self.H.T+self.R)
         self.x=x_pred+K@(z-self.H@x_pred)
         self.P=P_pred-K@self.H@P_pred
         return self.x
--- sensor_fusion/sensor_test.py
+++ sensor_fusion/sensor_test.py
@@ -4,20 +4,20 @@ import matplotlib.pyplot as plt
 from time import sleep
 
 sensor=MPU6050()
-phi,theta,psi=0,0,0
+phi_gyro,theta_gyro,psi_gyro=0,0,0
 phis_gryo=[0]
 thetas_gyro=[0]
 psis_gyro=[0]
 dt=0.1
 while len(psis_gyro)<60/dt:
         Ax,Ay,Az,Gx,Gy,Gz=sensor.read_value()
-        phi,theta,psi=np.array([[1,np.sin(phi)*np.tan(theta),np.cos(phi)*np.tan(theta)],
-                        [0,np.cos(phi),-np.sin(phi)],
-                        [0,np.sin(phi)/np.cos(theta),np.cos(phi)/np.cos(theta)]])@np.array([Gx,Gy,Gz]).T
-        phis_gryo.append(phis_gryo[-1]+phi*dt)
-        thetas_gyro.append(thetas_gyro[-1]+theta*dt)
-        psis_gyro.append(psis_gyro[-1]+psi*dt)
-        print(f'phi: {phi}, theta: {theta}, psi:{psi}')
+        phi_gyro,theta_gyro,psi_gyro=np.array([[1,np.sin(phi_gyro)*np.tan(theta_gyro),np.cos(phi_gyro)*np.tan(theta_gyro)],
+                        [0,np.cos(phi_gyro),-np.sin(phi_gyro)],
+                        [0,np.sin(phi_gyro)/np.cos(theta_gyro),np.cos(phi_gyro)/np.cos(theta_gyro)]])@np.array([Gx,Gy,Gz]).T
+        phis_gryo.append(phis_gryo[-1]+phi_gyro*dt)
+        thetas_gyro.append(thetas_gyro[-1]+theta_gyro*dt)
+        psis_gyro.append(psis_gyro[-1]+psi_gyro*dt)
+        print(f'phi: {phi_gyro}, theta: {theta_gyro}, psi:{psi_gyro}')
         sleep(dt)
         
 
